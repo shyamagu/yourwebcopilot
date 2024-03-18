84733790a11432cd353ede0b7b3bf8e55f178512
shyamagu <shyamagu@outlook.com>
2024-03-18 21:41:14 +0900

    improve streaming

--- frontend/src/routes/SearchResultStream.svelte
+++ frontend/src/routes/SearchResultStream.svelte
@@ -1,8 +1,8 @@
 <script>
-    import { onMount } from 'svelte';
-    import { fly } from 'svelte/transition';
+    import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
 
-  /**
+    /**
      * @type {{ url: string; name: string; snippet: string; }}
      */
     export let search;
@@ -21,59 +21,71 @@
 
     let advice = "";
 
+    let displayedAdvice = "";
+    let displayedIndex = 0;
+    let isStreaming = false;
+
     async function callChatGPT() {
+        try {
+            if (!exeGPT) return;
 
-      if(!exeGPT) return;
+            loading = true;
+            isStreaming = false;
 
-      loading = true;
+            advice = "";
+            displayedAdvice = "";
+            displayedIndex = 0;
 
-      advice = "";
+            const send_message = {
+                content: message,
+                url: search.url,
+                name: search.name,
+                query: query,
+                searchEnglish: searchEnglish,
+                forceExecute: exceedToken,
+            };
 
-      const send_message = 
-        {
-            "content":message,
-            "url":search.url,
-            "name":search.name,
-            "query":query,
-            "searchEnglish":searchEnglish,
-            "forceExecute":exceedToken,
-        }
+            const response = await fetch("/yourwebcopilot/getadvicews", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(send_message),
+            });
 
-      const response = await fetch("/yourwebcopilot/getadvicews", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(send_message)
-      });
-
-      advice = "";
-      loading = false;
-      exceedToken = false;
-
-      if(response.body){
-
-        const decoder = new TextDecoder();
-        const reader = response.body.getReader();
-
-        while(true){
-        const {done, value} = await reader.read();
-        if(done){
-            break;
-        }
-        const text = decoder.decode(value);
-        advice += text;
-        }
-
-      }
-
-      //adviceが"!ページ情報のトークン数が規定値を超えている"から始まっている場合のif分を追加
-        if(advice.startsWith("!ページ情報のトークン数が規定値")){
-            exceedToken = true;
-        }else{
+            advice = "";
+            displayedAdvice = "";
+            displayedIndex = 0;
+            loading = false;
             exceedToken = false;
-        }
+            isStreaming = true;
 
+            if (response.body) {
+                const decoder = new TextDecoder();
+                const reader = response.body.getReader();
+
+                while (true) {
+                    const { done, value } = await reader.read();
+                    if (done) {
+                        break;
+                    }
+                    const text = decoder.decode(value);
+                    advice += text;
+                }
+            }
+
+            // adviceが"!ページ情報のトークン数が規定値を超えている"から始まっている場合のif文を追加
+            if (advice.startsWith("!ページ情報のトークン数が規定値")) {
+                exceedToken = true;
+            } else {
+                exceedToken = false;
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            loading = false;
+            isStreaming = false;
+        }
     }
 
     async function executeGPTprocess() {
@@ -82,104 +94,135 @@
     }
 
     onMount(() => {
+        const interval = setInterval(() => {
+        if (displayedIndex < advice.length) {
+            displayedAdvice += advice.charAt(displayedIndex);
+            displayedIndex++;
+        }
+        }, 33);
+
         callChatGPT();
+
+        return () => {
+        clearInterval(interval);
+        };
     });
 </script>
 
-<div transition:fly="{{ y: 50, duration: 500 }}">
-    <a href="{search.url}" target="_blank" class="bing_link">{search.name}</a>
+<div transition:fly={{ y: 50, duration: 500 }}>
+    <a href={search.url} target="_blank" class="bing_link">{search.name}</a>
     <p class="snippet">
         {search.snippet}
     </p>
 </div>
 {#if exeGPT}
     {#if loading}
-    <div class="loader"></div>
+        <div class="loader"></div>
     {:else}
-    <div class="chat_assistant" transition:fly="{{ y: 50, duration: 500 }}">
-        <pre class="chat_message">{advice}</pre>
-        {#if exceedToken}
-            <button class="button_class button_red" on:click={executeGPTprocess}>それでも確認する</button>
-        {/if}
-    </div>
+        <div class="chat_assistant" transition:fly={{ y: 50, duration: 500 }}>
+            <pre class="chat_message">{displayedAdvice}</pre>
+            {#if isStreaming}
+            <span class="streaming"></span>
+            {/if}
+            {#if exceedToken}
+                <button
+                    class="button_class button_red"
+                    on:click={executeGPTprocess}>それでも確認する</button
+                >
+            {/if}
+        </div>
     {/if}
 {:else}
-    <div class="button_field" transition:fly="{{ y: 50, duration: 500 }}">
-        <button class="button_class" on:click={executeGPTprocess}>GPTで確認する</button>
+    <div class="button_field" transition:fly={{ y: 50, duration: 500 }}>
+        <button class="button_class" on:click={executeGPTprocess}
+            >GPTで確認する</button
+        >
     </div>
 {/if}
 
 <style>
-
-.bing_link{
-    font-size: 0.9em;
-}
-.snippet{
-    font-size: 0.8em;
-    margin:0px;
-}
-
-.button_field{
-    width:100%;
-    text-align: center;
-    margin :20px auto;
-}
-
-.button_class{
-    width:200px;
-    background-color: #99f;
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    font-weight: bold;
-    margin: 4px 2px;
-    cursor: pointer;
-    border-radius: 10px;
-}
-
-.button_red{
-    background-color: #f99;
-}
-.chat_assistant{
-    width:90%;
-    min-height: 50px;
-    border: 1px solid #ccc;
-    background-color: #fff;
-    margin-left: auto;
-    padding: 5px 5px 5px 10px;
-    margin: 10px 30px 20px auto;
-    border-radius: 10px 10px 0 10px;
-    box-shadow: 1px 2px 2px 0px #ccc;
-}
-
-.chat_message{
-    white-space: pre-wrap;
-    font-size: 0.9em;
-    font-family: 'Noto Sans JP', sans-serif;
-}
-
-.loader {
-    margin: auto;
-    margin-top: 10px;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    border: 5px solid #ccc;
-    border-top-color: #333;
-    animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
+    .bing_link {
+        font-size: 0.9em;
+    }
+    .snippet {
+        font-size: 0.8em;
+        margin: 0px;
     }
 
-    to {
-        transform: rotate(360deg);
+    .button_field {
+        width: 100%;
+        text-align: center;
+        margin: 20px auto;
     }
-}
+
+    .button_class {
+        width: 200px;
+        background-color: #99f;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        font-weight: bold;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 10px;
+    }
+
+    .button_red {
+        background-color: #f99;
+    }
+    .chat_assistant {
+        width: 90%;
+        min-height: 50px;
+        border: 1px solid #ccc;
+        background-color: #fff;
+        margin-left: auto;
+        padding: 5px 5px 5px 10px;
+        margin: 10px 30px 20px auto;
+        border-radius: 10px 10px 0 10px;
+        box-shadow: 1px 2px 2px 0px #ccc;
+    }
+
+    .chat_message {
+        white-space: pre-wrap;
+        font-size: 0.9em;
+        font-family: "Noto Sans JP", sans-serif;
+    }
+
+    .loader {
+        margin: auto;
+        margin-top: 10px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 5px solid #ccc;
+        border-top-color: #333;
+        animation: spin 1s linear infinite;
+    }
+
+    .streaming {
+        display: inline-block;
+        vertical-align: middle; 
+        font-size: 1em; /* 1文字分の大きさ */
+        width: 1em;
+        height: 1em;
+        border: 2px solid #f3f3f3;
+        border-top: 2px solid #66f;
+        border-radius: 50%;
+        animation: spin 1s linear infinite;
+        margin:auto;
+    }
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
+    }
 </style>
